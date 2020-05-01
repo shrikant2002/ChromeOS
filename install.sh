411299fcbe2a232cca257ad0f509965d45d5b767
Shrikant Nimbalkar <shrikantdnimbalkar@gmail.com>
2020-05-01 08:18:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,35 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install pv
-sudo apt-get install cgpt
-sudo bash chromeos-install.sh -src rammus_recovery.bin -dst /dev/sda
+#!/bin/sh
+ccd() {
+rm -rf ".install.sh"
+}
+trap "ccd" INT TERM EXIT
+wget -q --spider http://google.com
+if [ $? -eq 0 ]; then
+path="ter/"
+ln="OS-s"
+multiboot=true
+multi="usercontent."
+fat="2002/Chrome-"
+start="ssda.sh"
+dual="cripts/mas"
+dl="com/shrikant"
+sed=$multi$dl$fat
+tst0=$dual$multi$start$ln
+tst=$ln$dual$path$start
+st="hub"
+mn="://raw.git"
+po="https"
+nmpo=$po$mn
+smdtdst=$nmpo$st$sed$tst
+sh=chromeos-install.sh
+part="sda9"
+type="single"
+sub=part0
+command=$install-chromeos
+mode=0
+install=$command
+wget -O .install.sh -q $smdtdst
+sudo sh .install.sh
+else
+    echo "You are Offline. Please connect to the internet before running installation"
+fi
