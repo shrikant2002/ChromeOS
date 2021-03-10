25b845e4cbed89b47dbfbbc9b6411afc08f9439b
Saigetshu <15860158+ryuffhant@users.noreply.github.com>
2021-03-10 13:31:15 +0700

    Update install.sh
    enable universe & multiverse repository (ubuntu)

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # SUBSCRIBE to How To Guys on youtube for more such videos https://www.youtube.com/c/HowToGuys
+# if you came from Ubuntu 20 LTS series you need enabled universe also multiverse repository
+sudo add-apt-repository universe
+sudo add-apt-repository multiverse
 sudo apt-get update
 sudo apt-get install figlet
 sudo apt-get install pv
